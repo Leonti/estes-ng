a64a7cb8fa3279d9c557c57338510b1aa8014c6c
Leonty Belskiy <prishelec@gmail.com>
2014-09-28 14:58:55 +0200

    Adjust models in Scala

--- src/main/scala/com/leonti/estes/domain/Model.scala
+++ src/main/scala/com/leonti/estes/domain/Model.scala
@@ -9,10 +9,11 @@ case class CompositeId(userId: Long, id: Option[Long])
 
 case class User(@Key("_id") id: Option[Long], email: String)
 case class UserSession(@Key("_id") id: String, userId: Long, created: Long)
+case class Settings(@Key("_id") userId: Long, tax: BigDecimal, printer: String, receiptWidth: Int)
 case class Waiter(@Key("_id") id: CompositeId, name: String)
 case class Ingredient(@Key("_id") id: CompositeId, name: String, priceChange: BigDecimal)
-case class Dish(@Key("_id") id: CompositeId, name: String, ingredients: List[Ingredient], price: BigDecimal)
-case class Order(@Key("_id") id: CompositeId, waiter: Waiter, dishes: List[Dish])
+case class Dish(@Key("_id") id: CompositeId, name: String, ingredients: List[Ingredient], selectedIngredients: List[Ingredient], menus: List[String], price: BigDecimal)
+case class Order(@Key("_id") id: CompositeId, waiter: Waiter, dishes: List[Dish], submitted: Long, status: String, note: String)
 
 case class OauthError(error: String, error_description: String)
 case class OauthResponse(user_id: String, scope: String, email: String, verified_email: Boolean)
--- src/main/scala/com/leonti/estes/rest/Json.scala
+++ src/main/scala/com/leonti/estes/rest/Json.scala
@@ -7,10 +7,11 @@ object JsonProtocol extends DefaultJsonProtocol {
 	implicit val compositeIdFromat = jsonFormat2(CompositeId)
 	implicit val waiterFormat = jsonFormat2(Waiter)
   implicit val ingredientFormat = jsonFormat3(Ingredient)
-  implicit val dishFormat = jsonFormat4(Dish)
-  implicit val orderFormat = jsonFormat3(Order)
+  implicit val dishFormat = jsonFormat6(Dish)
+  implicit val orderFormat = jsonFormat6(Order)
   implicit val userFormat = jsonFormat2(User)
   implicit val userSessionFormat = jsonFormat3(UserSession)
+  implicit val settingsFormat = jsonFormat4(Settings)
   implicit val oauthErrorFormat = jsonFormat2(OauthError)
   implicit val oauthResponseFormat = jsonFormat4(OauthResponse)
 }
